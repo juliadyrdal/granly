3327b706c7d30282b84db4132f24d75b5c6f86d9
juliadyrdal <juliadyrdal@protonmail.com>
2023-06-23 17:40:39 +0100

    debug: remove photo swipe gallery

--- components/PhotoGrid.vue
+++ components/PhotoGrid.vue
@@ -1,41 +1,41 @@
 <template>
     <div>
-        <vue-picture-swipe :items="photos">
-        </vue-picture-swipe>
+        <!-- <vue-picture-swipe :items="photos">
+        </vue-picture-swipe> -->
     </div>
 </template>
 
 <script setup>
-import VuePictureSwipe from 'vue3-picture-swipe';
+// import VuePictureSwipe from 'vue3-picture-swipe';
 
-const photoFileRoot = ref('images/gallery/granly')
+// const photoFileRoot = ref('images/gallery/granly')
 
-const photoFileNames = ref([
+// const photoFileNames = ref([
 
-])
+// ])
 
 
 
-const photos = ref([
+// const photos = ref([
 
-])
+// ])
 
-function formatFileNames() {
-    for (let i = 1; i < 126; i++) {
-        photoFileNames.value.push(photoFileRoot.value + i)
-    }
+// function formatFileNames() {
+//     for (let i = 1; i < 126; i++) {
+//         photoFileNames.value.push(photoFileRoot.value + i)
+//     }
     
-}
+// }
 
 
-function generatePhotoObjects() {
-    formatFileNames()
-    for (let i = 0; i < 125; i++) {
-        photos.value.push({src: photoFileNames.value[i] + '.jpg', thumbnail: photoFileNames.value[i] + '.jpg', w: 1200, h: 900, title: 'Stue'})
-    }
-}
+// function generatePhotoObjects() {
+//     formatFileNames()
+//     for (let i = 0; i < 125; i++) {
+//         photos.value.push({src: photoFileNames.value[i] + '.jpg', thumbnail: photoFileNames.value[i] + '.jpg', w: 1200, h: 900, title: 'Stue'})
+//     }
+// }
 
-generatePhotoObjects()
+// generatePhotoObjects()
 
 //     {src: 'images/gallery/_DSC1407.jpg',thumbnail: 'images/gallery/_DSC1407.jpg',w: 1200, h: 900, title: 'Stue'},
 </script>
