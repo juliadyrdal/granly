ce85505e98b5ceb4f18be3275a8620951f1aac18
juliadyrdal <47177325+juliadyrdal@users.noreply.github.com>
2023-06-25 14:24:42 +0000

    codesandbox test

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,64 +1,116 @@
 <template>
-    <header class="bg-white">
-      <nav class="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8" aria-label="Global">
-        <div class="flex lg:flex-1">
-          <NuxtLink to="/" class="-m-1.5 p-1.5">
+  <!-- Code sandbox test -->
+  <header class="bg-white">
+    <nav
+      class="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div class="flex lg:flex-1">
+        <NuxtLink to="/" class="-m-1.5 p-1.5">
+          <span class="sr-only">Granly</span>
+          <p
+            class="text-2xl font-bold uppercase font-display leading-6 text-theme-grey-dark"
+          >
+            Granly
+          </p>
+        </NuxtLink>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+        <NuxtLink
+          v-for="item in navigation"
+          :key="item.name"
+          :to="item.href"
+          class="text-sm uppercase font-normal leading-6 text-theme-grey-dark"
+          >{{ item.name }}</NuxtLink
+        >
+      </div>
+      <div class="flex flex-1 items-center justify-end gap-x-6">
+        <a
+          href="#"
+          class="border border-theme-grey-dark px-4 py-2 text-sm font-semibold uppercase text-theme-grey-dark shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >Contact</a
+        >
+      </div>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          @click="mobileMenuOpen = true"
+        >
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+    </nav>
+    <Dialog
+      as="div"
+      class="lg:hidden"
+      @close="mobileMenuOpen = false"
+      :open="mobileMenuOpen"
+    >
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel
+        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      >
+        <div class="flex items-center gap-x-6">
+          <a href="#" class="-m-1.5 p-1.5">
             <span class="sr-only">Granly</span>
-            <p class="text-2xl font-bold uppercase font-display leading-6 text-theme-grey-dark">Granly</p>
-          </NuxtLink>
-        </div>
-        <div class="hidden lg:flex lg:gap-x-12">
-          <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="text-sm uppercase font-normal leading-6 text-theme-grey-dark">{{ item.name }}</NuxtLink>
-        </div>
-        <div class="flex flex-1 items-center justify-end gap-x-6">
-          <a href="#" class="border border-theme-grey-dark px-4 py-2 text-sm font-semibold uppercase text-theme-grey-dark shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Contact</a>
-        </div>
-        <div class="flex lg:hidden">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+            <img
+              class="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+          <a
+            href="#"
+            class="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >Contact</a
+          >
+          <button
+            type="button"
+            class="-m-2.5 rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = false"
+          >
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-      </nav>
-      <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-        <div class="fixed inset-0 z-10" />
-        <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div class="flex items-center gap-x-6">
-            <a href="#" class="-m-1.5 p-1.5">
-              <span class="sr-only">Granly</span>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-            </a>
-            <a href="#" class="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Contact</a>
-            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-              <span class="sr-only">Close menu</span>
-              <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div class="mt-6 flow-root">
-            <div class="-my-6 divide-y divide-gray-500/10">
-              <div class="space-y-2 py-6">
-                <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</NuxtLink>
-              </div>
-              <div class="py-6">
-                <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
-              </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <NuxtLink
+                v-for="item in navigation"
+                :key="item.name"
+                :to="item.href"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                @click="mobileMenuOpen = false"
+                >{{ item.name }}</NuxtLink
+              >
+            </div>
+            <div class="py-6">
+              <a
+                href="#"
+                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >Log in</a
+              >
             </div>
           </div>
-        </DialogPanel>
-      </Dialog>
-    </header>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { Dialog, DialogPanel } from '@headlessui/vue'
-  import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
-  
-  const navigation = [
-    { name: 'Photo gallery', href: 'photogallery' },
-    { name: 'Features', href: '#' },
-    { name: 'Details of Sale', href: '#' },
-  ]
-  
-  const mobileMenuOpen = ref(false)
-  </script>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+  { name: "Photo gallery", href: "photogallery" },
+  { name: "Features", href: "#" },
+  { name: "Details of Sale", href: "#" },
+];
+
+const mobileMenuOpen = ref(false);
+</script>
