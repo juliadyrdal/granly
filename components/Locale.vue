484cd0b9c6bfea43ca0d6db3d129cb7e3e291817
juliadyrdal <47177325+juliadyrdal@users.noreply.github.com>
2023-06-27 09:38:19 +0000

    style locale component

--- components/Locale.vue
+++ components/Locale.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="mx-auto flex max-w-7xl py-2 px-6 lg:px-8">
-    <div></div>
-    <form class="ml-auto">
-      <select v-model="locale">
-        <option value="en">en</option>
-        <option value="no">no</option>
-      </select>
-    </form>
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto flex py-1.5 border-b border-gray-900/10">
+      <div></div>
+      <form class="ml-auto">
+        <select
+          v-model="locale"
+          class="text-sm font-sans font-light uppercase text-slate-500 underline underline-offset-2"
+        >
+          <option value="en">en</option>
+          <option value="no">no</option>
+        </select>
+      </form>
+    </div>
   </div>
 </template>
 
